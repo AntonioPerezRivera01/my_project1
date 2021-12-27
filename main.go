3a1fbdb942db1ddcf604b36e2beff97e99bc3e19
Antonio Pérez <antonio.perez@ikontechnologies.com>
2021-12-26 21:11:36 -0600

    Se quitaron 2 funciones

--- main.go
+++ main.go
@@ -19,15 +19,4 @@ func main() {
 	for x := 0; x < 10; x++ {
 		log.Println("Valor: " + string(x))
 	}
-
-	animal := "Lobo"
-	switch animal {
-	case "Lobo":
-		log.Println("Es un Lobo")
-		break
-	case "Perro":
-		log.Println("Es un Perro")
-		break
-
-	}
 }
